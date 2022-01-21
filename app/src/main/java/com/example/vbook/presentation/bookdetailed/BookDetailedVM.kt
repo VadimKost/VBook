45aae0c3ff9cd0a55304a17f921441b3aa5dce49
VadimKost <Vadim0810vk>
2022-01-21 13:51:49 +0200

    service unBind fix

--- app/src/main/java/com/example/vbook/presentation/bookdetailed/BookDetailedVM.kt
+++ app/src/main/java/com/example/vbook/presentation/bookdetailed/BookDetailedVM.kt
@@ -1,10 +1,12 @@
 package com.example.vbook.presentation.bookdetailed
 
 import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.vbook.bindService
-import com.example.vbook.domain.usecases.GetFilledBook
+import com.example.vbook.domain.usecases.GetFilledBookUseCase
 import com.example.vbook.presentation.mediaservice.MediaService
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
@@ -12,9 +14,7 @@ import kotlinx.coroutines.launch
 import com.example.vbook.domain.common.Result
 import com.example.vbook.isSuccess
 import com.example.vbook.presentation.common.UiState
-import com.google.android.exoplayer2.SimpleExoPlayer
 import dagger.hilt.android.qualifiers.ApplicationContext
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.withContext
@@ -23,35 +23,44 @@ import javax.inject.Inject
 
 @HiltViewModel
 class BookDetailedVM @Inject constructor(
-    private val getFilledBook: GetFilledBook,
-    @ApplicationContext context: Context
-): ViewModel() {
-    init {
-        bindService(context){
-            _serviceState.value=UiState.Success(it)
-        }
-    }
+    private val getFilledBook: GetFilledBookUseCase,
+    @ApplicationContext val context: Context
+) : ViewModel() {
     private var _serviceState = MutableStateFlow<UiState<MediaService>>(UiState.Loading)
     var serviceState = _serviceState.asStateFlow()
 
-    fun setServiceBook(bookUrl: String){
+    private var serviceConnection: ServiceConnection? = null
+
+    init {
+        viewModelScope.launch {
+            val (service, connection) = bindService(context)
+            _serviceState.value = UiState.Success(service)
+            serviceConnection = connection
+        }
+    }
+
+    fun setServiceBook(bookUrl: String) {
         viewModelScope.launch(Dispatchers.IO) {
-            when(val book = getFilledBook(bookUrl)){
-                is Result.Success-> {
-                    withContext(Dispatchers.Main){
+            when (val book = getFilledBook(bookUrl)) {
+                is Result.Success -> {
+                    withContext(Dispatchers.Main) {
                         _serviceState.value.isSuccess {
                             it.data.setBook(book.data)
-                            it.data.mediaNotificationManager.showNotificationForPlayer(it.data.player)
                         }
 
                     }
                 }
                 is Result.Error -> {
-                    _serviceState.value.isSuccess {
-                        it.data.onError(book.message)
-                    }
+                    _serviceState.value = UiState.Error(book.message)
                 }
             }
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        serviceConnection?.also {
+            context.unbindService(it)
         }
     }
 }
--- app/src/main/java/com/example/vbook/utils.kt
+++ app/src/main/java/com/example/vbook/utils.kt
@@ -15,73 +15,69 @@ import com.example.vbook.presentation.mediaservice.MediaService
 import com.google.gson.JsonSyntaxException
 import java.lang.IllegalStateException
 import java.time.Duration
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
-fun ThrowableResult(e:Throwable, tag:String="defError"): Result.Error {
+fun ThrowableResult(e: Throwable, tag: String = "defError"): Result.Error {
     Log.e("UseCase $tag", e.stackTraceToString())
-    return when(e){
-        is IllegalStateException -> Result.Error(e.message+" Error")
+    return when (e) {
+        is IllegalStateException -> Result.Error(e.message + " Error")
         is JsonSyntaxException -> Result.Error("Parse Error")
         else -> Result.Error("Other Shit From ThrowableResult")
     }
 }
 
 
-fun Book.isDetailed():Boolean{
+fun Book.isDetailed(): Boolean {
     return mp3List != null
 }
 
-fun BookEntity.isDetailed():Boolean{
+fun BookEntity.isDetailed(): Boolean {
     return mp3List != null
 }
 
-fun <T> getStateData(state:UiState<T>): T? {
-    if (state is UiState.Success){
-        return state.data
-    }else{
-        return null
-    }
-}
 @Composable
-fun <T> UiState<T>.IsSuccess(action: @Composable (UiState.Success<T>) -> Unit){
-    if (this is UiState.Success<T>){
+fun <T> UiState<T>.IsSuccess(action: @Composable (UiState.Success<T>) -> Unit) {
+    if (this is UiState.Success<T>) {
         action(this)
     }
 }
 
-fun <T> UiState<T>.isSuccess(action:(UiState.Success<T>) -> Unit){
-    if (this is UiState.Success<T>){
+fun <T> UiState<T>.isSuccess(action: (UiState.Success<T>) -> Unit) {
+    if (this is UiState.Success<T>) {
         action(this)
     }
 }
 
-fun Long.toTime():String{
+fun Long.toTime(): String {
     val minutes = this / 1000 / 60
     val seconds = this / 1000 % 60
-    if (seconds<10){
+    if (seconds < 10) {
         return "$minutes:0$seconds"
     }
     return "$minutes:$seconds"
 }
 
-fun Long.toSliderFloat(duration: Long):Float{
-    if (this != 0L && duration!=0L){
-        return ((this*100).toFloat())/duration.toFloat()
-    }
-    else return 0f
+fun Long.toSliderFloat(duration: Long): Float {
+    if (this != 0L && duration != 0L) {
+        return ((this * 100).toFloat()) / duration.toFloat()
+    } else return 0f
 
 }
 
 
-fun bindService(context:Context,action:(MediaService)->Unit){
-    context.bindService(Intent(context, MediaService::class.java),object : ServiceConnection {
-        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-            val playerServiceBinder = service as MediaService.PlayerServiceBinder
-            val service = playerServiceBinder.service
-            action(service)
-        }
+suspend fun bindService(context: Context): Pair<MediaService, ServiceConnection> {
+    return suspendCoroutine { continuation ->
+        context.bindService(Intent(context, MediaService::class.java), object : ServiceConnection {
+            override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+                val playerServiceBinder = service as MediaService.PlayerServiceBinder
+                val service = playerServiceBinder.service
+                continuation.resume(Pair(service, this))
+            }
 
-        override fun onServiceDisconnected(name: ComponentName?) {
-        }
-    }, Context.BIND_AUTO_CREATE)
+            override fun onServiceDisconnected(name: ComponentName?) {
+            }
+        }, Context.BIND_AUTO_CREATE)
+    }
 }
 
